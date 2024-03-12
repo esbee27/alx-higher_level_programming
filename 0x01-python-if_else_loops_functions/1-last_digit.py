51f55fa1944c117dc9d22ee72e7407ce2f857f06
Blessing Ogor <ogorblessing96@gmail.com>
2024-03-12 15:18:11 -0700

    Last digits

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number >= 0:
+    modulus = number % 10
+else:
+    modulus = number % -10
+
+if modulus > 5:
+    print(f"Last digit of {number} is {modulus} and is greater than 5")
+elif modulus == 0:
+    print(f"Last digit of {number} is {modulus} and is equal to 0")
+elif modulus < 6:
+    print(f"Last digit of {number} is {modulus} and is less than 6 and not 0")
