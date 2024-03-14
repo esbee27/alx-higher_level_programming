a9ea99973c9e2bd092beca32761f32e166d17474
Blessing Ogor <ogorblessing96@gmail.com>
2024-03-13 20:18:48 -0700

    TAsks 8 to 13

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for num in range(1, 101):
-        if num % 3 = 0:
-            print(f"Fizz")
-        elif num % 5 = 0:
-            print(f"Buzz")
-        elif num % 3 = 0 and num % 5 = 0:
-            print(f"FizzBuzz")
+        if num % 3 == 0:
+            print(f"Fizz ", end='')
+        elif num % 5 == 0:
+            print(f"Buzz ", end='')
+        elif num % 3 == 0 and num % 5 == 0:
+            print(f"FizzBuzz ", end='')
         else:
-            print(f"{num} ")
+            print(f"{num} ", end='')
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number <= 0:
+    if number >= 0:
         last_digit = number % 10
     else:
-        last_digit = number % -10
-    print(f"{ld}")
-    rteurn (last_digit)
+        last_digit = number % 10
+    print(f"{last_digit}", end='')
+    return (last_digit)
 
 
