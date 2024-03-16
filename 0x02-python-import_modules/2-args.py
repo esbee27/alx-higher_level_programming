fdd942b14de4b64f411c274547827b92bba00970
Blessing <ogorblessing96@gmail.com>
2024-03-16 12:11:59 +0100

    Task 1 to 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_len = len(sys.argv)
+
+    if arg_len == 1:
+        print("0 arguments.")
+    elif arg_len == 2:
+        print("1 argument:")
+    else
+    print("{} arguments:".format(arg_len - 1))
+
+    for i in range(1, arg_len):
+        print("{}: {}".format(i, sys.argv[i]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+
+    arg_len = len(sys.argv)
+    for i in range(1, arg_len)
+    result += int(sys.argv[i])
+    print(f"{result}")
