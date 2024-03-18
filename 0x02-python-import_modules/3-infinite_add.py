120f9e7a3936480beebdda057a2638741ebddb7c
Blessing <ogorblessing96@gmail.com>
2024-03-18 20:23:18 +0100

    All tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     result = 0
 
     arg_len = len(sys.argv)
-    for i in range(1, arg_len)
-    result += int(sys.argv[i])
-    print(f"{result}")
+    for i in range(1, arg_len):
+        result += int(sys.argv[i])
+        :x:print(f"{result}")
