f8139612a96b1bde8145d86d51b83708f50e7931
Blessing Ogor <ogorblessing96@gmail.com>
2024-03-18 21:06:21 -0700

    Print no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string))
-    if my_string[i] != 'c' or my_string[i] != 'C':
-        print(f"{my_string}")
+    length = len(my_string)
+
+    j = 0
+    new_string = my_string[:]
+
+    for i in range(length):
+        if my_string[i] != 'C' or my_string[i] != 'c':
+            return(new_string)
