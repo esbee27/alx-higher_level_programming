6ef473fbcf7d368b9b80cb3b9275107d7dbee93a
Blessing Ogor <ogorblessing96@gmail.com>
2024-03-20 01:26:12 -0700

    prints a variable

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    str = dir90
+    for i in range(0, len(str)):
+        if str[i][0:2] != "__":
+            print("{}".format(str[i]))
