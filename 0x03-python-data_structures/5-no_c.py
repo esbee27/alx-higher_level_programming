625ada431341aa4656fcd866bc208aeff86ca020
Blessing Ogor <ogorblessing96@gmail.com>
2024-03-20 00:56:36 -0700

    Deletes char c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    length = len(my_string)
-
-    j = 0
-    new_string = my_string[:]
-
-    for i in range(length):
-        if my_string[i] != 'C' or my_string[i] != 'c':
-            return(new_string)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return(new_string)
