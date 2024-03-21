c05d74a099d7d76bbc00fa86a55bcdf687dba4d8
Blessing Ogor <ogorblessing96@gmail.com>
2024-03-20 21:31:06 -0700

    Tasks 2 to 5

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    x = new_list.replace(search, replace)
+    x = my_list.replace('search', 'replace')
     print(x)
