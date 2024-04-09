6924293f73ac48b4a62d1e94ba4c4a83a5141251
joynkem <ogorblessing96@gmail.com>
2024-04-09 01:31:57 +0100

    Tasks 0 to 2 done

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a triangle
+Defines a rectangle class
+"""
+
+
+
+class Rectangle:
+    """An empty rectangle class"""
+
+
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Module 1-rectangle
+Defines a rectangle
+"""
+
+
+class Rectangle:
+    """A class that defines a rectangle"""
+
+
+
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle.
+
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Defines the width of the private class width
+        A private class
+
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets value of self
+
+        Args:
+            value: value of the width
+
+        """
+
+        if type(width) != int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def width(self):
+        """Defines height of the private class width
+        A private class
+
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets value of self
+
+        Args:
+            value: value of the height
+
+        """
+
+        if type(height) != int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
