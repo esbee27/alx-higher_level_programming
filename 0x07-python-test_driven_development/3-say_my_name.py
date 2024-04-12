ac2fc771d8e90b1cebb3529cb5b992ce5f8feb2c
Blessing <ogorblessing96@gmail.com>
2024-04-12 13:29:41 +0100

    Corrected task 2 and 4

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A module that prints first name and last name
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    raises type erroe if firstname or last_name
+    is not a string
+    """
+
+    if type(first_name) is not string:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not string:
+        raise TypeError("first_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
