115c4df7a46a45d888913183b1a0d36346df90a1
Blessing <ogorblessing96@gmail.com>
2024-04-12 10:26:04 +0100

    Corrected task 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A module that adds 2 integers
+It returns the addition of the integers which is an integer
+"""
+
+def add_integer(a, b=98):
+    """A function that adds two integers
+    It checks if the arguments are integers of floats
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a is not an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b is not an integer')
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return a + b
