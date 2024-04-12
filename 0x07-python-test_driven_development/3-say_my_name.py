e1d65368b4dacf2612f204e36ff42913cd27b81d
Blessing Ogor <ogorblessing96@gmail.com>
2024-04-12 05:48:35 -0700

    Corrected task 4

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,8 +9,8 @@ def say_my_name(first_name, last_name=""):
     is not a string
     """
 
-    if type(first_name) is not string:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) is not string:
+    if type(last_name) is not str:
         raise TypeError("first_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
