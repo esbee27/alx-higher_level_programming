e2f4982d6b6915ece45921130762496d288a5307
Blessing <ogorblessing96@gmail.com>
2024-04-18 16:29:14 +0100

    Corrected a typo

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Returns the python structure from json"""
+"""Returns the python
+structure from json
+"""
 
 import json
 
 def from_json_string(my_str):
     """Returns the python structure"""
 
-    return json.load(my_string)
+    return json.loads(my_str)
