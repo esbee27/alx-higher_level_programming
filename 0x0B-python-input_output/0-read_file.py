2a953d6e8721633d39745474beb32adefda546f2
Blessing <ogorblessing96@gmail.com>
2024-04-18 16:23:53 +0100

    Corrected task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,12 @@ The file is filename
 """
 
 def read_file(filename=""):
-    with open(filename, encoding = "UTF-8") as f:
+    """Reads a text file and prints to stdout
+
+    Args:
+        filename: the file to be read
+    """
+
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
