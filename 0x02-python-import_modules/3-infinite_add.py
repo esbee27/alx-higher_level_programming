4f49db37d3d8549ad77d7100cf5109c2ccb16d1d
Blessing Ogor <ogorblessing96@gmail.com>
2024-05-01 06:37:32 -0700

    Corrected task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,6 @@ if __name__ == "__main__":
     import sys
     result = 0
 
-    arg_len = len(sys.argv)
-    for i in range(1, arg_len):
+    for i in range(1, len(sys.argv)):
         result += int(sys.argv[i])
-        print("{}".format(result))
+    print("{}".format(result))
