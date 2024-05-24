c56cd2d752f031f2f8573e37885d03eb4701faf0
Blessing Ogor <ogorblessing96@gmail.com>
2024-05-23 19:45:19 -0700

    Finished project

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- Removesbfrom second table, scoee<= 5
+DELETE FROM second_table WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- Prints all namge and score except where name is NULL
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
