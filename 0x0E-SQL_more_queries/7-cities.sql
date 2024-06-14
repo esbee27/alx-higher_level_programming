7b0bbffb077b127e934c0966587cb68628d9b98a
Blessing Ogor <ogorblessing96@gmail.com>
2024-06-14 06:24:53 -0700

    Corrected tasks 6 and 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,9 @@
 -- Creates database
 -- Creates table states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_us
-a;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	state_id NOT NULL INT,
-	name VARCHAR(256)) NOT NULL,
-	FOREIGN KEY (states_id),
-	REFERENCES states(id));
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (states_id) REFERENCES states(id));
