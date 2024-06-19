742d024f21648c6337831a8c212da8b54ed4e4dd
Blessing Ogor <ogorblessing96@gmail.com>
2024-06-18 17:33:22 -0700

    Tasks 8 to 11 done

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Lists all ciyies if Califonia
-SELECT cities FROM hbtn_0d_usa.states
+SELECT id, name FROMcities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY cities.id;;
