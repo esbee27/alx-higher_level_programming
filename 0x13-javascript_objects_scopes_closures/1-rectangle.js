b9b3858a94fc4227c6b12cfce3b4353be4a8d04f
Blessing Ogor <ogorblessing96@gmail.com>
2024-06-20 22:07:46 -0700

    Tasks 0 and1 done

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,10 @@
 //Defines and constructs the class, Rectangle
 
 class Rectangle {
-	constructor(w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
+
 module.exports = Rectangle
