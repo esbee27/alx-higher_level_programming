cb89dec04501b77ad8ff705dda9f66e3f4364b87
Blessing Ogor <ogorblessing96@gmail.com>
2024-07-08 19:00:43 -0700

    Tasks 1 and 2 done

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter"""
+from urllib import parse, request
+from sys import argv
+
+url = argv[1]
+values = {'email': argv[2]}
+eml = parse.urlencode(values)
+data = eml.encode('ascii')
+req = request.Request(url, data)
+with request.urlopen(req) as response:
+    r = response.read()
+    print(r.decode("utf-8"))
