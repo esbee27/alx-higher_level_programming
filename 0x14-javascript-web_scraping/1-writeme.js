72c744fcb9544f7f327d968ed4dc8c45f6ad61e4
Blessing Ogor <ogorblessing96@gmail.com>
2024-07-30 22:16:18 -0700

    Tasks done

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const data = process.argv[3];
 const file = process.argv[2];
 
-fs.writeFle(file, data, 'utf-8', (err) => {
-  if (err) {
-    console.log(err);
-  });
+fs.writeFile(file, data, 'utf-8', err => {
+  if (err) console.log(err);
+   });
